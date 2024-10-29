43442ba328d056843f17b6f07b3e8c766c41fba9
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-10-29 20:53:26 +0100

    Create first views

--- images_processor/views.py
+++ images_processor/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePage(TemplateView):
+    template_name = 'homepage.html'
+
+
+class AddMetadata(TemplateView):
+    template_name = 'add_metadata.html'
+
+
+class GenerateCSV(TemplateView):
+    template_name = 'generate_csv.html'
+
+
+class CaptionsGenerateCSV(TemplateView):
+    template_name = 'captions_generate_csv.html'
