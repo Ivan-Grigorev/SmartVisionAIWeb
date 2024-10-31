0992cbce6b8ef924975fbfb611dcba6aff2c728a
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-10-31 22:14:40 +0100

    Renamed project folder from SmartVisionAI to SmartVisionAIWeb

--- smartvisionaiweb_site/wsgi.py
+++ smartvisionaiweb_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for smartvisionai_site project.
+WSGI config for smartvisionaiweb_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartvisionai_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartvisionaiweb_site.settings")
 
 application = get_wsgi_application()
